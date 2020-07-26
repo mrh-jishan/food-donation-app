d8d4d0fbbe555c14f1257b973a09bb29725dc9d0
robin-hassan <robiul.hassan12102@gmail.com>
2020-07-26 23:25:38 +0800

    added food

--- src/pages/PostFood.js
+++ src/pages/PostFood.js
@@ -1,6 +1,8 @@
 import { Picker } from '@react-native-community/picker';
+import firestore from '@react-native-firebase/firestore';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 import { Button, TextInput } from 'react-native-paper';
 
 class PostFood extends React.Component {
@@ -8,20 +10,42 @@ class PostFood extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            name: '',
             dataPosted: new Date().toLocaleString(),
-            contact: '020394837',
-            email: 'user1@gmai.com',
-            password: 'User121032',
-            type: 'donor'
+            type: '',
+            manfDateVal: '',
+            expfDateVal: '',
+            coverage: '',
+            description: '',
+            manfData: false,
+            expDate: false,
         }
+    }
+
+    toggleManfDate = (value) => {
+        this.setState({ manfData: !this.state.manfData, manfDateVal: value.dateString });
+    }
+
+    toggleExpDate = (value) => {
+        this.setState({ expDate: !this.state.expDate, expfDateVal: value.dateString });
+    }
+
+    postFoodHandle = () => {
+        firestore().collection('Foods').add(this.state).then(res => {
+            this.props.navigation.navigate('DonorDashboard');
+        }).catch(err => {
+
+        });
     }
 
     render() {
         return (
-            <View style={styles.container}>
+            <ScrollView style={styles.container}>
                 <Text style={{ padding: 20 }}>This is Post Food page</Text>
                 <Text>Date Posted: {this.state.dataPosted}</Text>
-                <TextInput placeholder="Food Name"  style={styles.textInput}/>
+                <TextInput placeholder="Food Name" 
+                 onChangeText={text => this.setState({ name: text })}
+                style={styles.textInput} />
                 <View style={{
                     borderRadius: 5,
                     borderWidth: 1,
@@ -40,15 +64,44 @@ class PostFood extends React.Component {
                         onValueChange={(itemValue, itemIndex) =>
                             this.setState({ type: itemValue })
                         }>
-                        <Picker.Item label="Select User Type" value="" />
-                        <Picker.Item label="Donor" value="donor" />
-                        <Picker.Item label="Receiver" value="receiver" />
+                        <Picker.Item label="Select Food Type" value="" />
+                        <Picker.Item label="Raw" value="raw" />
+                        <Picker.Item label="Cooked" value="cooked" />
                     </Picker>
                 </View>
-                <TextInput placeholder="Food Coverage" style={styles.textInput}/>
-                <TextInput multiline={true} numberOfLines={4} placeholder="Food Description"/>
-                <Button mode="contained" style={{margin: 10}}>Post Food</Button>
-            </View>
+
+                <TouchableOpacity onPress={() => this.toggleManfDate('')}>
+                    <TextInput
+                        placeholder="Manufactured Date"
+                        style={styles.textInput}
+                        value={this.state.manfDateVal}
+                        disabled={true} />
+                </TouchableOpacity>
+                {this.state.manfData && (
+                    <Calendar onDayPress={value => this.toggleManfDate(value)} />
+                )}
+
+                <TouchableOpacity onPress={() => this.toggleExpDate('')}>
+                    <TextInput
+                        placeholder="Expiry Date"
+                        style={styles.textInput}
+                        value={this.state.expfDateVal}
+                        disabled={true} />
+                </TouchableOpacity>
+                {this.state.expDate && (
+                    <Calendar onDayPress={value => this.toggleExpDate(value)} />
+                )}
+
+                <TextInput
+                    placeholder="Food Coverage"
+                    onChangeText={text => this.setState({ coverage: text })}
+                    style={styles.textInput} />
+                <TextInput multiline={true} numberOfLines={4}
+                 placeholder="Food Description"
+                 onChangeText={text => this.setState({ description: text })}
+                 />
+                <Button mode="contained" style={{ margin: 10 }} onPress={this.postFoodHandle}>Post Food</Button>
+            </ScrollView>
         )
     }
 }
