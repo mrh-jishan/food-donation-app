54bce51f7b6589d20ff56033c52f7cfeaf854a2c
robin-hassan <robiul.hassan12102@gmail.com>
2020-08-09 01:17:26 +0800

    added qr code

--- src/components/QRdialog.js
+++ src/components/QRdialog.js
@@ -1,23 +1,29 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import { Button, Paragraph, Dialog, Portal } from 'react-native-paper';
+import { Button, Dialog, Portal } from 'react-native-paper';
+import QRCode from 'react-native-qrcode-svg';
 
-const QRdialog = ({visible,showDialog, hideDialog, food}) => {
-  return (
-    <View>
-      <Portal>
-        <Dialog visible={visible} onDismiss={hideDialog}>
-          <Dialog.Title>Scan this QR</Dialog.Title>
-          <Dialog.Content>
-  <Paragraph>{JSON.stringify(food)}</Paragraph>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={hideDialog}>Done</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </Portal>
-    </View>
-  );
+const QRdialog = ({ visible, showDialog, hideDialog, food }) => {
+    return (
+        <View>
+            <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Title>Scan this QR</Dialog.Title>
+                    <Dialog.Content style={{ alignSelf: 'center' }}>
+                        <QRCode
+                            logoSize={350}
+                            size={200}
+                            value={JSON.stringify(food)}
+                        />
+
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button onPress={hideDialog}>Done</Button>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
+        </View>
+    );
 };
 
 export default QRdialog;
